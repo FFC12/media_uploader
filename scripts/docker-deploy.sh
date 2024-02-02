cdbf14d52f34d482a3c69c15917491103ba51fe0
mutex <fatihsaika@gmail.com>
2024-02-02 17:46:53 +0300

    docker-deploy.sh

--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
+
 cd ../
+
+# Build the Docker image
 docker build --pull --rm -f "Dockerfile" -t mediauploader:latest "."
+
+# Run the Docker container in detached mode
 docker run -d -p 8080:8080 -i -t mediauploader
-docker exec -i mediauploader bash -c "cd app && ./server -useSpawnerWithMemoryLimit=true -enableSimpleInterface=true -addr=0.0.0.0:8080"
